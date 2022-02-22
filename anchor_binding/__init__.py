17f5a68eb3e4e50725ec2fb684179a1c7497fec3
paavo <shibaev.pp@phystech.edu>
2022-02-22 20:54:16 +0300

    add docstrings

---
+++ anchor_binding/__init__.py
@@ -0,0 +1,1 @@
+"""anchor_binding makes calls to Anchor Protocol to retrieve APY etc."""
--- anchor_binding/anchor.py
+++ anchor_binding/anchor.py
@@ -1,12 +1,17 @@
+"""Anchor_binding makes calls to Anchor Protocol to retrieve APY etc."""
 import subprocess
 import json
 
 
 class AnchorAPI:
+    """To use package import this class..."""
+
     def __init__(self, bin_path="./app"):
+        """Intialize with a binary tool..."""
         self._bin_path = bin_path
 
     def get_balance(self):
+        """Query protocol APY..."""
         process = subprocess.Popen(self._bin_path.split(),
                                    stdout=subprocess.PIPE)
         output, error = process.communicate()
