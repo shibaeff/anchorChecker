176fe309d38044816e7e9560fc8ee5f142ed1ad0
paavo <shibaev.pp@phystech.edu>
2022-02-22 17:59:12 +0300

    implement get_balance

--- anchor_binding/anchor.py
+++ anchor_binding/anchor.py
@@ -1,10 +1,12 @@
 import subprocess
+import json
+
 
 class AnchorAPI:
     def __init__(self, bin_path):
         self._bin_path = bin_path
 
-    def GetBalance(self):
-        bashCommand = "bin_path"
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    def get_balance(self):
+        process = subprocess.Popen(self._bin_path.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
+        return json.loads(output)
