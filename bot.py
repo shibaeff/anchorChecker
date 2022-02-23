46698c28dab8d3e60e98f01c5c5b128cf3630247
Ilya Savitsky <ipsavitsky234@gmail.com>
2022-02-23 23:18:05 +0300

    added docstrings to bot functions

--- bot.py
+++ bot.py
@@ -19,6 +19,9 @@ class BotStates(StatesGroup):
 
 @bot.message_handler(commands=['start'])
 async def poll_threshhold(message):
+    """
+    write the prompt to enter threshhold, set state to recieving the answer
+    """
     # TODO: gather theese two tasks
     await bot.set_state(message.from_user.id, BotStates.registration, message.chat.id)
     await bot.send_message(message.chat.id, 'Input your threshold:')
@@ -26,12 +29,18 @@ async def poll_threshhold(message):
 
 @bot.message(state=BotStates.registration)
 async def register_user(message):
+    """
+    add user to database(or not, lol)
+    """
     # WARNING: если добавлять еще стейты(вроже коннекта с кошельком, то надо добавлять через retrieve_data(типа как тут https://github.com/eternnoir/pyTelegramBotAPI/blob/master/examples/asynchronous_telebot/custom_states.py))
     # TODO: проверить что похоже на число и/или процент регэкспом
     users[message.sender] = float(message.text)
 
 
 async def run_notifications():
+    """
+    query the Anchor API and go through users to send if APY drops
+    """
     balance = AnchorAPI().get_balance()
     for user, threshhold in users.items():
         if balance['APY'] < threshhold:
@@ -39,12 +48,18 @@ async def run_notifications():
 
 
 async def scheduler_process():
+    """
+    routine to query notifications-wait an hour-repeat
+    """
     while True:
         await run_notifications()
         await asyncio.sleep(3600)
 
 
 async def main():
+    """
+    main loop
+    """
     await asyncio.gather(bot.infinity_polling(), scheduler_process())
 
 asyncio.run(main())
