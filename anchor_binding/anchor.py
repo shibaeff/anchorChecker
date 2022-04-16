74eb3c38aa70ee0e0b821640a8a10b8c9409fff4
paavo <shibaev.pp@phystech.edu>
2022-04-16 21:38:59 +0300

    clean the docstrings

--- anchor_binding/anchor.py
+++ anchor_binding/anchor.py
@@ -1,24 +1,23 @@
-"""anchor module provides a proxy to Anchor Protocol API
-"""
+"""anchor module provides a proxy to Anchor Protocol API"""
 
 import subprocess
 import json
 
+
 class AnchorAPI:
-    """A proxy class to the Anchor Protocol
+    """
+    A proxy class to the Anchor Protocol
 
     :param bin_path: path to anchor_tool binary
     :type: str, optional
     """
 
     def __init__(self, bin_path="./app"):
-        """Intialize with a binary tool
-        """
+        """Intialize with a binary tool"""
         self._bin_path = bin_path
 
     def get_balance(self):
-        """Query protocol APY - annual percentage yield
-        """
+        """Query protocol APY - annual percentage yield"""
         process = subprocess.Popen(self._bin_path.split(),
                                    stdout=subprocess.PIPE)
         output, error = process.communicate()
--- source/__main__.py
+++
@@ -1,9 +0,0 @@
-"""AnchorBot bot entry point..."""
-import logging
-from bot import *
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    logging.info("Anchor bot app started...")
-    bot.add_custom_filter(asyncio_filters.StateFilter(bot))
-    asyncio.run(main())
