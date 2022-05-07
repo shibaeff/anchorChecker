cf05ffc153e7e7c65ec192834813fe1aeae4180f
paavo <shibaev.pp@phystech.edu>
2022-05-07 18:57:13 +0300

    fix docker compose

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 ssh $1 ls -l
 ssh $1 git clone https://github.com/shibaeff/anchorChecker
 scp -r .env $1:~/anchorChecker/.env
-ssh $1 docker-compose -f ./anchorChecker/docker-compose.yaml up -d anchorbot
+ssh $1 cd ./anchorChecker &&  docker-compose up -d anchorbot
