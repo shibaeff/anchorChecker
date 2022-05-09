79f6e18f0d5723018247cc97f76addde53a7d55e
paavo <shibaev.pp@phystech.edu>
2022-05-09 19:03:03 +0300

    fix pipenv

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 ssh $1 ls -l
 ssh $1 git clone https://github.com/shibaeff/anchorChecker
 scp -r .env $1:~/anchorChecker/.env
+ssh $1 pybabel compile -D bot -d po -l ru
 ssh $1 cd ./anchorChecker &&  docker-compose up -d anchorbot
