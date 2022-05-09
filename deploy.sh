57a3867390633326b3e113f41ae8acac0c207dec
paavo <shibaev.pp@phystech.edu>
2022-05-09 19:45:34 +0300

    edit docker compose

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 ssh $1 ls -l
 ssh $1 git clone https://github.com/shibaeff/anchorChecker
+ssh pybabel compile -D bot -d po -l ru
 scp -r .env $1:~/anchorChecker/.env
-ssh $1 pybabel compile -D bot -d po -l ru
-ssh $1 cd ./anchorChecker &&  docker-compose up -d anchorbot
+scp ./po/ru/LC_MESSAGES/bot.mo $1:~/anchorChecker/po/ru/LC_MESSAGES/bot.mo
+ssh $1 docker-compose -f ./anchorChecker/docker-compose.yaml up -d
